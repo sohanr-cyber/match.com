2df3b3b52c8a721eaf6b681263cb21c65b16c16e
sohanr-cyber <sohanur25800@gmail.com>
2023-12-27 14:38:02 +0600

    sjlfjdsljf

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let BASE_URL
 if (process.env.NODE_ENV !== 'production') {
   BASE_URL = 'http://localhost:3000'
 } else {
-  BASE_URL = 'https://muslimmatch.vercel.app'
+  BASE_URL = 'https://match-com.vercel.app/'
 }
 
 const APP_SECRET = process.env.APP_SECRET
