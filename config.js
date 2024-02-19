922ddcede9146b0b0a4df936472f3ee39725b46a
sohanr-cyber <sohanur25800@gmail.com>
2024-02-19 22:00:04 +0600

    sdjgsdsdgdsgsdg

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ let BASE_URL
 if (process.env.NODE_ENV !== 'production') {
   BASE_URL = 'http://localhost:3000'
 } else {
-  // BASE_URL = 'https://match-com.vercel.app'
-  BASE_URL = 'https://main--match-com.netlify.app'
+  BASE_URL = 'https://match-com.vercel.app'
 }
 
 const APP_SECRET = process.env.APP_SECRET
