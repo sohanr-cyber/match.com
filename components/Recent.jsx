2d6c87df693b11338a8560d55ac4085b60eea9db
sohanr-cyber <sohanur25800@gmail.com>
2024-03-02 21:31:42 +0600

    recent

--- components/Recent.jsx
+++ components/Recent.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from '@/styles/Recent.module.css'
 import Card from '@/components/Profile/Card'
+import { useRouter } from 'next/router'
 
 const Recent = ({ recent }) => {
+  const router = useRouter()
+  const ln = router.locale
+
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.heading}>Recently Created Profile</h1>
+      {ln == 'bn' ? (
+        <h1 className={styles.heading}>সম্প্রতি তৈরি করা প্রোফাইল সমূহ</h1>
+      ) : (
+        <h1 className={styles.heading}>Recently Created Profile</h1>
+      )}
       <div className={styles.flex}>
         {recent.map((item, index) => (
           <Card key={index} index={index} user={item} />
