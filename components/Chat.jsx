8c8c811d1b2c6565232ccfda9da04d12716fb68e
sohanr-cyber <81401762+sohanr-cyber@users.noreply.github.com>
2024-08-03 23:46:51 +0600

    048504

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -6,17 +6,17 @@ import { useRouter } from 'next/router'
 const Chat = () => {
   const router = useRouter()
 
-
   return (
     <>
       <div className={styles.wrapper}>
         <div className={styles.chat__link}>
           <div
             className={styles.icon}
-            onClick={() => router.push('http://Wa.me/+8801744329811')}
+            // onClick={() => router.push('http://Wa.me/+8801744329811')}
+            onClick={() => router.push('http://m.me/61563578257362')}
           >
             <Image
-              src='https://cdn-icons-png.flaticon.com/128/3670/3670051.png'
+              src='https://cdn-icons-png.flaticon.com/128/5968/5968771.png'
               width='35'
               height='35'
               alt='inst'
